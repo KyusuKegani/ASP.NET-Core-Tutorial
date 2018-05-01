8fd8d78d098fcfee398d447da731a9ce1181e2f5
Yusuke Sabi <nekomastery050313@gmail.com>
2018-05-02 00:09:30 +0900

    ASP.NET Core　チュートリアル第2章まで

--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,8 @@ namespace RazorPagesMovie
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+
         }
 
         public IConfiguration Configuration { get; }
